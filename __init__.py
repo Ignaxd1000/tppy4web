858185147e4075aa3d6db49ff8902a24246eb3bf
Ignaxd1000 <nachoursobaez@gmail.com>
2025-10-17 21:06:19 -0300

    lito

--- __init__.py
+++ __init__.py
@@ -1,15 +1,3 @@
-"""
-Tutorial para API web e Interfaz móvil
-ET 29 DE 6 "Reconquista de Buenos Aires"
-
-Copyright 2025 Alan Edmundo Etkin
-
-Por la presente se concede permiso, libre de cargos, a cualquier persona que obtenga una copia de este software y de los archivos de documentación asociados (el "Software"), a utilizar el Software sin restricción, incluyendo sin limitación los derechos a usar, copiar, modificar, fusionar, publicar, distribuir, sublicenciar, y/o vender copias del Software, y a permitir a las personas a las que se les proporcione el Software a hacer lo mismo, sujeto a las siguientes condiciones:
-
-El aviso de copyright anterior y este aviso de permiso se incluirán en todas las copias o partes sustanciales del Software.
-EL SOFTWARE SE PROPORCIONA "COMO ESTÁ", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O IMPLÍCITA, INCLUYENDO PERO NO LIMITADO A GARANTÍAS DE COMERCIALIZACIÓN, IDONEIDAD PARA UN PROPÓSITO PARTICULAR E INCUMPLIMIENTO. EN NINGÚN CASO LOS AUTORES O PROPIETARIOS DE LOS DERECHOS DE AUTOR SERÁN RESPONSABLES DE NINGUNA RECLAMACIÓN, DAÑOS U OTRAS RESPONSABILIDADES, YA SEA EN UNA ACCIÓN DE CONTRATO, AGRAVIO O CUALQUIER OTRO MOTIVO, DERIVADAS DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O SU USO U OTRO TIPO DE ACCIONES EN EL SOFTWARE.
-"""
-
 # check compatibility
 import py4web
 
@@ -26,5 +14,5 @@ from .tasks import scheduler
 
 # optional parameters
 __version__ = "1.0.0"
-__author__ = "Alan Etkin <spametki@gmail.com>"
-__license__ = "MIT. Ver archivo LICENCIA"
+__author__ = "Ignaxd1000"
+__license__ = "Ninguna"
